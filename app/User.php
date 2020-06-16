226491075ca4a54079408a32bddb9c51a7cf450c
John <tuannguyen0719@gmail.com>
2020-06-16 21:47:44 +0700

    laravel full text search example

--- app/User.php
+++ app/User.php
@@ -5,10 +5,25 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Nicolaslopezj\Searchable\SearchableTrait;
 class User extends Authenticatable
 {
     use Notifiable;
+    use SearchableTrait;
+
+
+    /**
+    * Searchable rules.
+    *
+    * @var array
+    */
+   protected $searchable = [
+       'columns' => [
+           'users.name' => 10,
+           'users.email' => 5,
+           'users.id' => 3,
+       ]
+   ];
 
     /**
      * The attributes that are mass assignable.
